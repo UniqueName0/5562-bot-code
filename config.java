0adac631b3ba0308c949d3b276fac4572cfaa060
UniqueName <75515676+UniqueName0@users.noreply.github.com>
2021-03-03 15:38:26 -0500

    Update config.java

--- config.java
+++ config.java
@@ -8,7 +8,7 @@ public static float MaxSpeed = (float) .25; //maxspeed, .8 = 80%, .5 = 50%. DO N
 public static float LauncherSpeed = (float) 1; //speed of the launcher, .8 = 80%, 1 = 100%. DO NOT SET OVER 1 OR BELOW 0
 
 
-public static float ReverseLauncherSpeed = (float) 75; //speed of the launcher when reversed, .8 = 80%, 1 = 100%. DO NOT SET OVER 1 OR BELOW 0
+public static float ReverseLauncherSpeed = (float) .75; //speed of the launcher when reversed, .8 = 80%, 1 = 100%. DO NOT SET OVER 1 OR BELOW 0
 
 
 public static float autonSpeed = (float) .3; //speed of motors in autonomous, .8 = 80%, .5 = 50%. DO NOT SET OVER 1 OR BELOW 0
